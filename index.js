300d825c189a84b5d1fd1718d28ab756c602d664
Grecia Delgado <greciadm2596@gmail.com>
2025-07-31 19:46:52 -0600

    update modules

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 (() => {
   var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __commonJS = (cb, mod) =>
-    function __require() {
-      return (
-        mod ||
-          (0, cb[__getOwnPropNames(cb)[0]])(
-            (mod = { exports: {} }).exports,
-            mod
-          ),
-        mod.exports
-      );
-    };
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
 
   // tareas.js
   var require_tareas = __commonJS({
@@ -28,38 +20,43 @@
       module.exports = {
         agregarTarea,
         borrarTareas,
-        obtenerTareas,
+        obtenerTareas
       };
-    },
+    }
   });
 
   // dom.js
-  var tareasMod = require_tareas();
-  var input = document.getElementById("nueva-tarea");
-  var buttonAgregar = document.getElementById("agregar");
-  var buttonBorrar = document.getElementById("borrar");
-  var ul = document.getElementById("tareas");
-  function mostrarTareas() {
-    ul.innerHTML = "";
-    var tareas = tareasMod.obtenerTareas();
-    for (const tarea of tareas) {
-      var li = document.createElement("li");
-      li.textContent = tarea;
-      ul.append(li);
+  var require_dom = __commonJS({
+    "dom.js"() {
+      var tareasMod = require_tareas();
+      var input = document.getElementById("nueva-tarea");
+      var buttonAgregar = document.getElementById("agregar");
+      var buttonBorrar = document.getElementById("borrar");
+      var ul = document.getElementById("tareas");
+      function mostrarTareas() {
+        ul.innerHTML = "";
+        var tareas = tareasMod.obtenerTareas();
+        for (const tarea of tareas) {
+          var li = document.createElement("li");
+          li.textContent = tarea;
+          ul.append(li);
+        }
+      }
+      function handleAgregar(_event) {
+        var nuevaTarea = input.value;
+        if (nuevaTarea === "") return;
+        tareasMod.agregarTarea(nuevaTarea);
+        input.value = "";
+        mostrarTareas();
+      }
+      function handleBorrar(_event) {
+        ul.innerHTML = "";
+        tareasMod.borrarTareas();
+      }
+      buttonAgregar.onclick = handleAgregar;
+      buttonBorrar.onclick = handleBorrar;
+      mostrarTareas();
     }
-  }
-  function handleAgregar(_event) {
-    var nuevaTarea = input.value;
-    if (nuevaTarea === "") return;
-    tareasMod.agregarTarea(nuevaTarea);
-    input.value = "";
-    mostrarTareas();
-  }
-  function handleBorrar(_event) {
-    ul.innerHTML = "";
-    tareasMod.borrarTareas();
-  }
-  buttonAgregar.onclick = handleAgregar;
-  buttonBorrar.onclick = handleBorrar;
-  mostrarTareas();
+  });
+  require_dom();
 })();
