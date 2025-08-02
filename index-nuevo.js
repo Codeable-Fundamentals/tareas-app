34c05285eaa4df2063478be390125cbfaa4ad7be
Grecia Delgado <greciadm2596@gmail.com>
2025-08-01 20:02:43 -0600

    update exmaple

--- index-nuevo.js
+++ index-nuevo.js
@@ -1,5 +1,18 @@
-import {
-  mostrarTareas,
-} from "./src/dom.js";
+import { mostrarTareas } from "./src/dom.js";
 
 mostrarTareas();
+
+let description = document.getElementById("descripcion");
+let parrafo = document.querySelector("p");
+
+console.log("que hay?", description);
+console.log("que selecionaste???", parrafo)
+
+description.style.color = "blue"
+
+console.log("aue es window.document: ", window.document)
+// window.alert("hola!!!!")
+
+// function saludar(){}
+
+// console.log("respuesta: ", saludar === window.saludar);
